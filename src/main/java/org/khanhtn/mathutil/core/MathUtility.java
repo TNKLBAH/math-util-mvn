220c5403b83fcc5b769084d37c1b23eadbae0b28
TNKLBAH <putinkhanh112@gmail.com>
2023-06-26 11:57:41 +0700

    update source to see green4ever

--- src/main/java/org/khanhtn/mathutil/core/MathUtility.java
+++ src/main/java/org/khanhtn/mathutil/core/MathUtility.java
@@ -21,18 +21,30 @@ public class MathUtility {
     //ko ap dung giai thua cho so am. Neu N < 0: LOI, EXCEPTION
     //ko giai thua cho so > 20. Vi 20! vua khit kieu long (18 so 0)
     //21!: LOI, EXCEPTION
+//    public static long getFactorial(int n) {
+//
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be between 0 and 20.");        
+//        
+//        if (n == 0 || n == 1) 
+//            return 1;        
+//        
+//        long product = 1; //tich khoi dau la 1, sau do nhan don vao
+//        for (int i = 2; i <= n; i++)
+//            product *= i;
+//        
+//        return product;
+//    }
     public static long getFactorial(int n) {
 
-        if (n < 0 || n > 20)
-            throw new IllegalArgumentException("Invalid n. n must be between 0 and 20.");        
-        
-        if (n == 0 || n == 1) 
-            return 1;        
-        
-        long product = 1; //tich khoi dau la 1, sau do nhan don vao
-        for (int i = 2; i <= n; i++)
-            product *= i;
-        
-        return product;
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("Invalid n. n must be between 0 and 20.");
+        }
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * getFactorial(n - 1);
     }
 }
